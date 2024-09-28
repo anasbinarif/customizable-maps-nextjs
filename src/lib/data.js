65d1205b20bc6abca3e42fc58bd06a1ac0cc02ba
anasbinarif <58784109+anasbinarif@users.noreply.github.com>
2024-09-28 17:29:55 +0200

    fix map error

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,14 +1,13 @@
-"use server"
 export const haversineDistance = (loc1, loc2) => {
-  const R = 3958.8; // Radius of Earth in miles
+  const R = 3958.8;
   const toRadians = (degrees) => degrees * (Math.PI / 180);
 
   const dLat = toRadians(loc2.lat - loc1.lat);
   const dLon = toRadians(loc2.lng - loc1.lng);
 
   const a =
-    Math.sin(dLat / 2) ** 2 +
-    Math.cos(toRadians(loc1.lat)) *
+      Math.sin(dLat / 2) ** 2 +
+      Math.cos(toRadians(loc1.lat)) *
       Math.cos(toRadians(loc2.lat)) *
       Math.sin(dLon / 2) ** 2;
 
